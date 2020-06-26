90a5fbb07455d8394f361e69062bedbc49d9d202
nawaf <princenawaf99@hotmail.com>
2020-06-26 22:02:03 +0300

    inital

---
+++ backend/testing.py
@@ -0,0 +1,23 @@
+# Import Python Package
+import jwt
+import base64
+
+
+# Init our Data
+payload = {'school':'udacity'}
+algo = 'HS256' #HMAC-SHA 256
+secret = 'learning'
+
+# Encode a JWT
+encoded_jwt = jwt.encode(payload, secret, algorithm=algo)
+print(encoded_jwt)
+
+print('----------------------------------------------------')
+# Decode a JWT
+decoded_jwt = jwt.decode(encoded_jwt, secret, verify=True)
+print(decoded_jwt)
+
+print('---------------------------------------------')
+# Decode with Simple Base64 Encoding
+decoded_base64 = base64.b64decode(str(encoded_jwt).split(".")[1]+"==")
+print(decoded_base64)
